d97216c01b3dd17bea55df8c7303c6f43dd85af5
hongshen.zhang <51727955+jkljk67890@users.noreply.github.com>
2021-04-20 15:04:29 +0800

    update another

---
+++ Another_Three_node_situation/Exact_Three_Node_1.m
@@ -0,0 +1,27 @@
+function [Mag_exact] = Exact_Three_Node_1
+%This part calculate the exact magnitude for two node situation
+%Here the node 1 is the generator bus
+%Here the node 2 is the load bus
+%Here the node 3 is the load bus
+%Suppose the power of load bus is S_load = mag + mag * j
+
+
+index_load = 3;
+iter = 1000;
+mag_bound = 0;
+mpopt = mpoption;
+mpopt = mpoption(mpopt,'PF_DC',0,'OUT_ALL',0,'VERBOSE',0);
+
+for loop = 1 : iter
+    mag = loop / iter;
+    mpc = case3;
+    mpc.bus(index_load , 3) = mag;
+    mpc.bus(index_load , 4) = mag;
+    results = runpf(mpc,mpopt);
+    if results.success == 1
+        mag_bound = mag;
+    end
+end
+
+Mag_exact = mag_bound;
+end
---
+++ Another_Three_node_situation/Form_YBUS.m
@@ -0,0 +1,14 @@
+function [YBUS] = Form_YBUS(mpc)
+
+%Fit the data type of YBUS of the paper 
+[YBUS, ~, ~] = makeYbus(mpc);
+Y_num = length(mpc.bus(:,1));
+for loop_i = 1 : Y_num
+    for loop_j = 1 : Y_num
+        if (loop_i ~= loop_j)
+            YBUS(loop_i,loop_j) = - YBUS(loop_i,loop_j);
+        end
+    end
+end
+
+end
