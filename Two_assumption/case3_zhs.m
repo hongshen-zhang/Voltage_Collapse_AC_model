a8a7ea757bcf91b6ba1c7237fe11d07c4ef5ceb1
hongshen.zhang <51727955+hongshen-zhang@users.noreply.github.com>
2021-05-06 21:14:13 +0800

    update case3_zhs

--- Two_assumption/case3_zhs.m
+++ Two_assumption/case3_zhs.m
@@ -1,4 +1,4 @@
-function mpc = case3_z
+function mpc = case3_zhs
 %CASE9    Power flow data for 9 bus, 3 generator case.
 %   Please see CASEFORMAT for details on the case file format.
 %
@@ -26,25 +26,25 @@ mpc.baseMVA = 1;
 %% bus data
 %	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin
 mpc.bus = [
-	1	2	0	0	0	0	1	1	0	345	1	1.5	0.5;
-    2	1	0.48	0	0	0	1	1	0	345	1	1.5	0.5;   
-	3	2	0	0	0	0	1	1	0	345	1	1.5	0.5;
+	1	3	0	0	0	0	1	1	0	0	1	1.5	0.5;
+    2	2	0.1	0.1	0	0	1	1	0	0	1	1.5	0.5;   
+	3	1	0.01	0.01	0	0	1	1	0	0	1	1.5	0.5;
 ];
 
 %% generator data
 %	bus	Pg	Qg	Qmax	Qmin	Vg	mBase	status	Pmax	Pmin	Pc1	Pc2	Qc1min	Qc1max	Qc2min	Qc2max	ramp_agc	ramp_10	ramp_30	ramp_q	apf
 mpc.gen = [
-	1	0	0	300	-300	1	1	1	250	0	0	0	0	0	0	0	0	0	0	0	0;
-    3	0	0	300	-300	1	1	1	250	0	0	0	0	0	0	0	0	0	0	0	0;
+	1	0 0	300	-300	1	1	1	250	-250	0	0	0	0	0	0	0	0	0	0	0;
+    2	0 0	300	-300	1	1	1	250	-250	0	0	0	0	0	0	0	0	0	0	0;
 ];
 
 %% branch data
 %	fbus	tbus	r	x	b	rateA	rateB	rateC	ratio	angle	status	angmin	angmax
 mpc.branch = [
-	1	2	1	0	0	0	0	0	0	0	1	-360	360;
-    2	3	1	0	0	0	0	0	0	0	1	-360	360;
-    1	3	1	0	0	0	0	0	0	0	1	-360	360;   
-];
+	1	2	1	1	0	0	0	0	0	0	1	-360	360;
+    2	3   100	100	0	0	0	0	0	0	1	-360	360;
+%    1	3	1	0	0	0	0	0	0	0	1	-360	360;
+    ];
 
 %%-----  OPF Data  -----%%
 %% generator cost data
